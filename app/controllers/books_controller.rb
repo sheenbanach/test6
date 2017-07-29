7e81707d71afc63da8118bcf2dfa6725d8cb5ab5
sheenbanach <daimon.fuji@gmail.com>
2017-07-29 18:02:28 +0900

    Homepage
    Homepage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -60,6 +60,9 @@ class BooksController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  def homepage
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
