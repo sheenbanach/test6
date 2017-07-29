08c602a686567ebd0058c160ea80490b034fc8d1
sheenbanach <daimon.fuji@gmail.com>
2017-07-29 18:01:02 +0900

    Route
    Route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get 'tuna/homepage'
   get 'fish/homepage'
   get 'books/homepage'
+  get 'toriki/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
